951c957b74e0dc651ff2abf3d95372da811bd7c8
KuroZephyr <djawed.m.mazari@gmail.com>
2023-06-04 14:29:50 +0100

    darkmod

---
+++ script.js
@@ -0,0 +1,11 @@
+function darkmod() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  var image = document.getElementById("dark");
+  if (image.getAttribute("src") === "./assets/darkmod.png") {
+    image.setAttribute("src", "./assets/lightmod.png");
+  } else {
+    image.setAttribute("src", "./assets/darkmod.png");
+  }
+  
+}
