23057308ee7cf9b51eaa1195a14e83db4e92fb8c
KuroZephyr <djawed.m.mazari@gmail.com>
2023-06-05 16:33:18 +0100

    update

--- script.js
+++ script.js
@@ -4,38 +4,29 @@ function darkmod() {
   var image = document.getElementById("dark");
   if (image.getAttribute("src") === "./assets/darkmod.png") {
     image.setAttribute("src", "./assets/lightmod.png");
+    document.querySelector(".heroText").setAttribute("dark", "");
+    // document.getElementsByTagName("form")[0].style.backgroundColor = "#252525";
+    // document.getElementsByClassName("images")[0].style.backgroundColor =
+    //   "#252525";
+    // document.getElementsByClassName("navbar")[0].style.fontWeight = "700";
   } else {
     image.setAttribute("src", "./assets/darkmod.png");
+    document.querySelector(".heroText").removeAttribute("dark");
   }
-    document.getElementsByClassName("heroText")[0].style.backgroundColor =
-    "#252525";
-    document.getElementsByTagName("form")[0].style.backgroundColor ="#252525"; 
-    document.getElementsByClassName("images")[0].style.backgroundColor ="#252525"; 
-    document.getElementsByClassName("navbar")[0].style.fontWeight ="700"; 
-    
 }
 
+var productItems = document.querySelectorAll(".product");
 
-
-
-
-
-
-var productItems = document.querySelectorAll('.product');
-          
-          
 productItems.forEach(function (item) {
-  
-  item.addEventListener('mouseover', function () {
-    
-    this.classList.add('animate');
+  item.addEventListener("mouseover", function () {
+    this.classList.add("animate");
   });
 
-  
-  item.addEventListener('mouseout', function () {
-    
-    this.classList.remove('animate');
+  item.addEventListener("mouseout", function () {
+    this.classList.remove("animate");
   });
 });
 
 lightGallery(document.querySelector(".gallery"));
+
+// I need dark mode
